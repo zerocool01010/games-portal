063e85630a9c7debeb2564c93c18e97466a90180
Manuel Alvarez <manuelalvarez_1993@hotmail.com>
2021-12-13 05:17:45 -0300

    Refactorizacion de codigo y agrego un condicional en playerPos para que no se pueda elegir mas de una posicion por juego

--- index4.js
+++ index4.js
@@ -1,29 +1,5 @@
 "use strict";
 
-/* Posibles mejoras de codigo repetido:
-En la funcion pickPos tengo este codigo muy similar para cada posicion:
-if (pos1 !== undefined/*  && pos2 == false *//* ) { //si existe...
-	console.log("this works too!");
-	let posX1, posY1 = undefined; //inicializo
-	let posiciones1 = calculatePos1(); //traigo las posiciones random de X e Y en formato json
-	for (const object of posiciones1){
-		if(object.posicionX1 !== undefined){
-			posX1 = object.posicionX1; //guardo la de X
-		}
-		if (object.posicionY1 !== undefined){
-			posY1 = object.posicionY1; //guardo la de Y
-		}
-	}
-	rectColour = ctx.fillStyle = "black"; //color negro al rect
-	playerPos(pos1, rectColour, posX1, posY1);  //le mando los params
-}*/
-/* Deberia poder hacer que ese codigo que es para la pos1 funcione para pos2 tambien
-El diferencial central está en que uno debe crear un rect negro y el otro blanco, y que una de las pos ingresadas en el
-select se guarda como pos1 y la otra como pos2. Podria preguntar en una condicion si es === a pos1 o === a pos2 para crear
-un rect negro o blanco. Esto ademas me permitira reducir calculatePos a una sola funcion.
-*/
-
-
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d"); //creo el "tablero" canvas
 let values1 = [] //aca se guardan las posiciones del J1
@@ -91,102 +67,75 @@ function pickPos(){
 	console.log("this works!");
 	console.log(pos1);
 	console.log(pos2);
-    if (pos1 !== undefined/*  && pos2 == false */) { //si existe...
-		console.log("this works too!");
-		let posX1, posY1 = undefined; //inicializo
-		let posiciones1 = calculatePos1(); //traigo las posiciones random de X e Y en formato json
-		for (const object of posiciones1){
-			if(object.posicionX1 !== undefined){
-				posX1 = object.posicionX1; //guardo la de X
-			}
-			if (object.posicionY1 !== undefined){
-				posY1 = object.posicionY1; //guardo la de Y
-			}
-		}
-        rectColour = ctx.fillStyle = "black"; //color negro al rect
-        playerPos(pos1, rectColour, posX1, posY1); //le mando los params
-    }
-    if (pos2 !== undefined/*  && pos1 == false */) { //idem arriba
-		console.log("this works too!");
-		let posX2, posY2 = undefined;
-		let posiciones2 = calculatePos2();
-		for (const object of posiciones2){
-			if(object.posicionX2 !== undefined){
-				posX2 = object.posicionX2;
-			}
-			if (object.posicionY2 !== undefined){
-				posY2 = object.posicionY2;
-			}
-		}
-        rectColour = ctx.fillStyle = "white"; //color blanco para el rect
-        playerPos(pos2, rectColour, posX2, posY2); //le mando los params
-    } 
+    
+	if (pos1 !== undefined && values1[0] === undefined) { //si existe...
+        rectColour = ctx.fillStyle = "black"; //color negro al rect del J1
+		let posiciones = calculatePos(); //traigo las posiciones random de X e Y en un array
+		playerPos(pos1, rectColour, posiciones[0], posiciones[1]); //le mando los params
+	} if (pos2 !== undefined && values2[0] === undefined){
+		rectColour = ctx.fillStyle = "white"; //color blanco al rect del J2
+		let posiciones = calculatePos(); //traigo las posiciones random de X e Y en un array
+		playerPos(pos2, rectColour, posiciones[0], posiciones[1]); //le mando los params
+	}
 }
-function calculatePos1(){
-	let xPP1, yPP1 = undefined; //inicializo
-	xPP1 = ((Math.random())*200); //calculo random para X (el resto de la medida se completa en playerPos)
-	yPP1 = ((Math.random())*150); //calculo random para Y (el resto de la medida se completa en playerPos)
-	let arrayObject1 = [{ //guardo los calculos en el json
-						"posicionX1": xPP1, 
-						"posicionY1": yPP1
-						}];
-	return arrayObject1; //lo devuelvo
+
+function calculatePos(){
+	let xP, yP = undefined; //inicializo
+	xP = ((Math.random())*200); //calculo random para X (el resto de la medida se completa en playerPos)
+	yP = ((Math.random())*150); //calculo random para Y (el resto de la medida se completa en playerPos)
+	let arrayPos = [xP, yP]; //guardo las pos en un arreglo
+	return arrayPos; //lo devuelvo
 }
-function calculatePos2(){ //idem arriba
-	let xPP2, yPP2 = undefined;
-	xPP2 = ((Math.random())*200);
-	yPP2 = ((Math.random())*150);
-	let arrayObject2 = [{
-						"posicionX2": xPP2, 
-						"posicionY2": yPP2
-						}];
-	return arrayObject2;
-}  
 
 function playerPos(pos, rectC, x, y){ //recibe los params del J1 o J2
-	rectC; //negro (J1) o blanco (J2). Debe traerse para que el rect generado pueda leer el color (trae el fillStyle)
-    try {
-        if (pos === "superior-izquierda") {
-			rect = ctx.fillRect(x, y, 20, 20); //crea el rect en las posiciones random por defecto
-			
-        } 
-        else if (pos === "superior-centro") {
-			x += 200;
-            rect = ctx.fillRect(x, y, 20, 20); //crea el rect en las posiciones random por defecto pero le suma 200 a la pos X
-			
-        }
-        else if (pos === "superior-derecha") {
-			x += 400;
-            rect = ctx.fillRect(x, y, 20, 20); //crea el rect en las posiciones random por defecto pero le suma 400 a la pos X
-			
-        }
-        else if (pos === "inferior-izquierda") {
-			y += 150;
-            rect = ctx.fillRect(x, y, 20, 20); //crea el rect en las posiciones random por defecto pero le suma 150 a la pos Y  
-        } 
-        else if (pos === "inferior-centro") {
-			x += 200;
-            y += 150;
-            rect = ctx.fillRect(x, y, 20, 20); //crea el rect en las posiciones random por defecto pero le suma 150 a la pos Y y 200 a la X
-        }
-        else if (pos === "inferior-derecha") {
-            x += 400;
-            y += 150;
-            rect = ctx.fillRect(x, y, 20, 20); //crea el rect en las posiciones random por defecto pero le suma 150 a la pos Y y 400 a la X
-        }
-    } catch (error) {
-        console.error("Wrong!");
-    }
-		console.log("La posición X del rect "+ rectC +" es: " + x);
-		console.log("La posición Y del rect "+ rectC +" es: " + y);
-		let object = { //guardo las posiciones traidas para J1 o J2 en este objecto
-			"xPos": x,
-			"yPos": y
+	if (pos !== ""){
+		rectC; //negro (J1) o blanco (J2). Debe traerse para que el rect generado pueda leer el color (trae el fillStyle)
+		try {
+			if (pos === "superior-izquierda") {
+				rect = ctx.fillRect(x, y, 20, 20); //crea el rect en las posiciones random por defecto
+				
+			} 
+			else if (pos === "superior-centro") {
+				x += 200;
+				rect = ctx.fillRect(x, y, 20, 20); //crea el rect en las posiciones random por defecto pero le suma 200 a la pos X
+				
+			}
+			else if (pos === "superior-derecha") {
+				x += 400;
+				rect = ctx.fillRect(x, y, 20, 20); //crea el rect en las posiciones random por defecto pero le suma 400 a la pos X
+				
+			}
+			else if (pos === "inferior-izquierda") {
+				y += 150;
+				rect = ctx.fillRect(x, y, 20, 20); //crea el rect en las posiciones random por defecto pero le suma 150 a la pos Y  
+			} 
+			else if (pos === "inferior-centro") {
+				x += 200;
+				y += 150;
+				rect = ctx.fillRect(x, y, 20, 20); //crea el rect en las posiciones random por defecto pero le suma 150 a la pos Y y 200 a la X
+			}
+			else if (pos === "inferior-derecha") {
+				x += 400;
+				y += 150;
+				rect = ctx.fillRect(x, y, 20, 20); //crea el rect en las posiciones random por defecto pero le suma 150 a la pos Y y 400 a la X
+			}
+		} catch (error) {
+			console.error("Wrong!");
 		}
-	if (rectC === "black"){ //si es del J1 === negro
-	values1.push(object); //las mando al array values para que luego puedan ser leidas para las condiciones de victoria
-	} else if (rectC === "white"){ //si es del J1 === negro
-	values2.push(object); //las mando al array values para que luego puedan ser leidas para las condiciones de victoria
+			console.log("La posición X del rect "+ rectC +" es: " + x);
+			console.log("La posición Y del rect "+ rectC +" es: " + y);
+			let object = { //guardo las posiciones traidas para J1 o J2 en este objecto
+				"xPos": x,
+				"yPos": y
+			}
+		if (rectC === "black"){ //si es del J1 === negro
+		values1.push(object); //las mando al array values para que luego puedan ser leidas para las condiciones de victoria
+		} else if (rectC === "white"){ //si es del J1 === negro
+		values2.push(object); //las mando al array values para que luego puedan ser leidas para las condiciones de victoria
+		}
+	} else {
+		console.error("No ingresada la posicion");
 	}
+	
 }
 
