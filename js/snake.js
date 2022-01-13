a83f2562ea1857c7b60f0a944bb12826e26f9bb5
Manuel Alvarez <manuelalvarez_1993@hotmail.com>
2022-01-13 17:37:46 -0300

    Snake funcionando con generacion de puertas. Faltan las condiciones de victoria o derrota

--- js/snake.js
+++ js/snake.js
@@ -7,7 +7,8 @@ let vueSnake = new Vue({
         positions: [],
 		savedHeight: 0,
 		savedWidth: 0,
-		escapeDoor: []
+		escapeDoor: [],
+		thereIsEscapeDoor: false
 	},
 	methods: {
 		/* getPrevR: function(){
@@ -28,53 +29,52 @@ function createSnake(width, height){
 		if (height > 0 || height < 0) {
 			vueSnake.savedHeight = height + vueSnake.savedHeight;
 			console.log("La nueva altura almacenada es de: " + vueSnake.savedHeight);
+			generateEscapeDoor();
 		}
 		if (width > 0 || width < 0){
 			vueSnake.savedWidth = width + vueSnake.savedWidth;
 			console.log("El nuevo ancho almacenado es de: " + vueSnake.savedWidth);
+			generateEscapeDoor();
 		}
     } else {
-        /* height = 1;
-		width = 1; */
-    }
+		let positOfDoor = generateEscapeDoor(); //genero la puerta de salida por primera vez //traigo un arreglo simple de dos posiciones (valores x e y de la puerta)
+		vueSnake.escapeDoor = positOfDoor;
+		console.log("Este sería el array que traigo con los valores X e Y de la puerta: " + vueSnake.escapeDoor[0] + " y " + vueSnake.escapeDoor[1]);
+		vueSnake.thereIsEscapeDoor = true;
+	}
 	console.log("El ancho que se sumó por param es de " + width);
 	console.log("La altura que se sumó por param es de " + height);
-    /* vueSnake.positions.pop(); */
+    
     ctx.fillStyle = "red";
     let redSnake = ctx.fillRect(vueSnake.savedWidth, vueSnake.savedHeight, 10, 40); //se genera el rectangulo rojo con las posiciones y sus medidas
-    /* console.log(redSnake); */
-    /* vueSnake.positions.push(redSnake); */
-    /* console.log(vueSnake.positions); */
+    
 }
 
 function deleteSnake(){
-    /* vueSnake.positions.pop(); */
+    
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-
-console.log("holaSet 5.5");
 
 function moveSnake(direction, quantity){
 	deleteSnake();
 	if (direction === "up" || direction === "down"){
 		let height = quantity;
 		if (direction === "down"){
-			/* let idInterval1 =  */setTimeout(createSnake, 2500, 0, height);//en el setInterval o setTimeOut 1° param es para function, 2do param para milisegundos, y 4°, 5° y otros params para params de la function
+			setTimeout(createSnake, 500, 0, height);//en el setInterval o setTimeOut 1° param es para function, 2do param para milisegundos, y 4°, 5° y otros params para params de la function
 			
-			/* clearInterval(idInterval1); */
 		} else if (direction === "up"){
 			let negHeight = Number(height * (-1));
-			setTimeout(createSnake, 2500, 0, negHeight);
+			setTimeout(createSnake, 500, 0, negHeight);
 		}
 		
 	} else {
 		let width = quantity;
 		if (direction === "right"){
-			/* let idInterval2 =  */setTimeout(createSnake, 2500, width, 0);
-			/* clearInterval(idInterval2); */
+			setTimeout(createSnake, 500, width, 0);
+			
 		} else if (direction === "left"){
 			let negWidth = Number(width * (-1));
-			setTimeout(createSnake, 2500, negWidth, 0);
+			setTimeout(createSnake, 500, negWidth, 0);
 		}
 		
 	}
@@ -88,6 +88,50 @@ function receiveValues(){
 	moveSnake(direction, quantity);
 }
 
+console.log("holaSet 5.92");
+
 function generateEscapeDoor(){
+	ctx.fillStyle = "blue";
+	if (vueSnake.thereIsEscapeDoor) { // si existe...
+		 if (vueSnake.escapeDoor[2] === "right") {
+			let blueDoorRect = ctx.fillRect(vueSnake.escapeDoor[0], vueSnake.escapeDoor[1], 10, 40);
+		} else { 
+			let blueDoorRect = ctx.fillRect(vueSnake.escapeDoor[0], vueSnake.escapeDoor[1], 40, 10);
+		} 
+	} else { //si no existe aun
+		let arrayXaY = doorPosit(); //traigo un array con las posiciones X e Y de la door
+		if (arrayXaY[2] === "bottom"){ //por la condicion que puse en doorPosit, X solo puede ser = a 397 si cae en el rightSide */
+			let blueDoorRect = ctx.fillRect(arrayXaY[0], arrayXaY[1], 10, 40);
+		} if (arrayXaY[2] === "right") {
+			let blueDoorRect = ctx.fillRect(arrayXaY[0], arrayXaY[1], 40, 10);
+		}
+		return arrayXaY; //la retorno
+	}
 	
+}
+
+function doorPosit(){
+	let randomSide = getRandomInt(); // traigo un 1 o un 2
+	if (randomSide == 1) {
+		let y = 397;
+		let x = (Math.random()) * 399;
+		/* if (x == 397) {
+			x += 1; //esto lo hago para evitar que pueda quedar en 397 y se pueda cumplir la condicion de control en generateEscapeDoor
+		} */
+		let bottomSide = [x, y, "bottom"];
+		return bottomSide;
+	} else {
+		let x = 397;
+		let y = (Math.random()) * 399;
+		let rightSide = [x, y, "right"];
+		return rightSide;
+	}
+}
+
+function getRandomInt() {
+	let max = 3;
+    let min = 1;	
+	let randNumb = Math.floor(Math.random() * (max - min)) + min; //devuelve por truncamiento un entero con el siguiente dominio {1; 2}
+	console.log("El numero random es: " +randNumb);
+	return randNumb;
 }
