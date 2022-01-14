267c2056dc5f35b91fb83d8056aeacf303865d82
Manuel Alvarez <manuelalvarez_1993@hotmail.com>
2022-01-14 17:57:48 -0300

    Modularizada la funcion para vigilar las condiciones de derrota frente a los obstaculos

--- js/snake.js
+++ js/snake.js
@@ -8,7 +8,10 @@ let vueSnake = new Vue({
 		savedHeight: 0,
 		savedWidth: 0,
 		escapeDoor: [],
-		thereIsEscapeDoor: false
+		thereIsEscapeDoor: false,
+		obstaclesSetUp: false,
+		obsPositions: [],
+		isTheGameOver: false
 	},
 	methods: {
 		/* getPrevR: function(){
@@ -25,29 +28,36 @@ document.querySelector("#move").addEventListener("click", receiveValues);
 document.querySelector("#delete").addEventListener("click", deleteSnake);
 
 function createSnake(width, height){
+	if (!(vueSnake.isTheGameOver)){
     if (height > 0 || height < 0 || width > 0 || width < 0){
 		if (height > 0 || height < 0) {
 			vueSnake.savedHeight = height + vueSnake.savedHeight;
 			console.log("La nueva altura almacenada es de: " + vueSnake.savedHeight);
 			generateEscapeDoor();
+			watchConditionsForLosing();
 		}
 		if (width > 0 || width < 0){
 			vueSnake.savedWidth = width + vueSnake.savedWidth;
 			console.log("El nuevo ancho almacenado es de: " + vueSnake.savedWidth);
 			generateEscapeDoor();
+			watchConditionsForLosing();
 		}
     } else {
 		let positOfDoor = generateEscapeDoor(); //genero la puerta de salida por primera vez //traigo un arreglo simple de dos posiciones (valores x e y de la puerta)
 		vueSnake.escapeDoor = positOfDoor;
 		console.log("Este sería el array que traigo con los valores X e Y de la puerta: " + vueSnake.escapeDoor[0] + " y " + vueSnake.escapeDoor[1]);
 		vueSnake.thereIsEscapeDoor = true;
+		renderAllTheObstacles();
+		vueSnake.obstaclesSetUp = true;
 	}
 	console.log("El ancho que se sumó por param es de " + width);
 	console.log("La altura que se sumó por param es de " + height);
     
     ctx.fillStyle = "red";
     let redSnake = ctx.fillRect(vueSnake.savedWidth, vueSnake.savedHeight, 10, 40); //se genera el rectangulo rojo con las posiciones y sus medidas
-    
+    } else {
+		console.log("Ya hay un resultado");
+	}
 }
 
 function deleteSnake(){
@@ -57,24 +67,25 @@ function deleteSnake(){
 
 function moveSnake(direction, quantity){
 	deleteSnake();
+	keepingAllTheObstaclesInCanvas();
 	if (direction === "up" || direction === "down"){
 		let height = quantity;
 		if (direction === "down"){
-			setTimeout(createSnake, 500, 0, height);//en el setInterval o setTimeOut 1° param es para function, 2do param para milisegundos, y 4°, 5° y otros params para params de la function
+			setTimeout(createSnake, 100, 0, height);//en el setInterval o setTimeOut 1° param es para function, 2do param para milisegundos, y 4°, 5° y otros params para params de la function
 			
 		} else if (direction === "up"){
 			let negHeight = Number(height * (-1));
-			setTimeout(createSnake, 500, 0, negHeight);
+			setTimeout(createSnake, 100, 0, negHeight);
 		}
 		
 	} else {
 		let width = quantity;
 		if (direction === "right"){
-			setTimeout(createSnake, 500, width, 0);
+			setTimeout(createSnake, 100, width, 0);
 			
 		} else if (direction === "left"){
 			let negWidth = Number(width * (-1));
-			setTimeout(createSnake, 500, negWidth, 0);
+			setTimeout(createSnake, 100, negWidth, 0);
 		}
 		
 	}
@@ -88,7 +99,7 @@ function receiveValues(){
 	moveSnake(direction, quantity);
 }
 
-console.log("holaSet 5.92");
+
 
 function generateEscapeDoor(){
 	ctx.fillStyle = "blue";
@@ -134,4 +145,61 @@ function getRandomInt() {
 	let randNumb = Math.floor(Math.random() * (max - min)) + min; //devuelve por truncamiento un entero con el siguiente dominio {1; 2}
 	console.log("El numero random es: " +randNumb);
 	return randNumb;
+}
+
+console.log("holaSet 6.5");
+
+function generateObstacles(obstacleAlreadySetUp){
+	ctx.fillStyle = "black";
+	if (!(vueSnake.obstaclesSetUp)) {
+	let objPos = randomPositionForCanvas(); //traigo un objeto con nombres "x" e "y" y posiciones aleatorias para el tablero
+	let blackObstacle = ctx.fillRect(objPos.x, objPos.y, 30, 30);
+	return objPos;
+	} else {
+	let blackObstacle = ctx.fillRect(obstacleAlreadySetUp.x, obstacleAlreadySetUp.y, 30, 30);
+	}
+}
+
+function renderAllTheObstacles(){
+	let obst0Position = generateObstacles();
+	let obst1Position = generateObstacles();
+	let obst2Position = generateObstacles();
+	let obst3Position = generateObstacles();
+	let obst4Position = generateObstacles();
+	vueSnake.obsPositions.push(obst0Position);
+	vueSnake.obsPositions.push(obst1Position);
+	vueSnake.obsPositions.push(obst2Position);
+	vueSnake.obsPositions.push(obst3Position);
+	vueSnake.obsPositions.push(obst4Position);
+}
+
+function keepingAllTheObstaclesInCanvas(){
+	if (vueSnake.obstaclesSetUp){
+		generateObstacles(vueSnake.obsPositions[0]);
+		generateObstacles(vueSnake.obsPositions[1]);
+		generateObstacles(vueSnake.obsPositions[2]);
+		generateObstacles(vueSnake.obsPositions[3]);
+		generateObstacles(vueSnake.obsPositions[4]);
+	}
+}
+
+function randomPositionForCanvas(){
+	let xPos = Math.random()*360;
+	let yPos = Math.random()*360;
+	let obj = {
+		"x": xPos,
+		"y": yPos
+	}
+	return obj;
+}
+
+function watchConditionsForLosing(){
+	for (const elem of vueSnake.obsPositions) {
+		if (vueSnake.savedWidth <= elem.x+30 && vueSnake.savedWidth+10 >= elem.x) {
+			if (vueSnake.savedHeight <= elem.y+30 && vueSnake.savedHeight+40 >= elem.y){
+				console.log("El jugador ha perdido contra un obstáculo");
+				vueSnake.isTheGameOver = true;
+			}
+		}
+	}
 }
