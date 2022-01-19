2d86fe58432067ff185745139cc77addbfc64f11
Manuel Alvarez <manuelalvarez_1993@hotmail.com>
2022-01-19 20:52:09 -0300

    Remodularizacion de funciones de comprobacion de victoria y derrota. Ademas de eso, se agregaron variables a Vue para reducir el algoritmo de ifs y ahora la snake se acuesta cuando va en horizontal

--- js/snake.js
+++ js/snake.js
@@ -4,14 +4,19 @@ let vueSnake = new Vue({
 	el: "#connector",
 	data: {
 		message: "Initializing Vue",
-        positions: [],
 		savedHeight: 0,
 		savedWidth: 0,
+		height: 0,
+		width: 0,
+		obstWH: 30,
+		doorWidth: 0,
+		doorHeight: 0,
 		escapeDoor: [],
 		thereIsEscapeDoor: false,
 		obstaclesSetUp: false,
 		obsPositions: [],
-		isTheGameOver: false
+		isTheGameOver: false,
+		vertical: false
 	},
 	methods: {
 		/* getPrevR: function(){
@@ -27,40 +32,46 @@ document.querySelector("#create").addEventListener("click", createSnake);
 document.querySelector("#move").addEventListener("click", receiveValues);
 /* document.querySelector("#delete").addEventListener("click", deleteSnake); */
 
-function createSnake(width, height){
+function createSnake(width, height, direction){
 	if (!(vueSnake.isTheGameOver)){
-    if (height > 0 || height < 0 || width > 0 || width < 0){
-		if (height > 0 || height < 0) {
-			vueSnake.savedHeight = height + vueSnake.savedHeight;
-			console.log("La nueva altura almacenada es de: " + vueSnake.savedHeight);
-			generateEscapeDoor();
-			watchConditionsForLosing();
-			watchConditionsForWinning()
+		gameStarting(width, height);
+		
+		ctx.fillStyle = "red";
+		if (direction === "up" || direction === "down"){
+			let redSnake = ctx.fillRect(vueSnake.savedWidth, vueSnake.savedHeight, vueSnake.width, vueSnake.height); //se genera el rectangulo rojo con las posiciones y sus medidas
+		} else {
+			let redSnake = ctx.fillRect(vueSnake.savedWidth, vueSnake.savedHeight, vueSnake.width, vueSnake.height); // si el movimient es horizontal la vibora se acuesta
 		}
-		if (width > 0 || width < 0){
-			vueSnake.savedWidth = width + vueSnake.savedWidth;
-			console.log("El nuevo ancho almacenado es de: " + vueSnake.savedWidth);
-			generateEscapeDoor();
-			watchConditionsForLosing();
-			watchConditionsForWinning()
-		}
-    } else {
-		let positOfDoor = generateEscapeDoor(); //genero la puerta de salida por primera vez //traigo un arreglo simple de dos posiciones (valores x e y de la puerta, y un tercer valor que indica si está al fondo o a la derecha)
-		vueSnake.escapeDoor = positOfDoor;
-		console.log("Este sería el array que traigo con los valores X e Y de la puerta: " + vueSnake.escapeDoor[0] + " y " + vueSnake.escapeDoor[1]);
-		vueSnake.thereIsEscapeDoor = true;
-		let dificultad = document.querySelector("#dificultad").value; //viene la dificultad ingresada por el usuario expresada en un numero
-		renderAllTheObstacles(dificultad-1); //el numero debe decir la cantidad de obstáculos en el juego
-		vueSnake.obstaclesSetUp = true;
-	}
-	console.log("El ancho que se sumó por param es de " + width);
-	console.log("La altura que se sumó por param es de " + height);
-    
-    ctx.fillStyle = "red";
-    let redSnake = ctx.fillRect(vueSnake.savedWidth, vueSnake.savedHeight, 10, 40); //se genera el rectangulo rojo con las posiciones y sus medidas
+		
     } else {
 		console.log("Ya hay un resultado");
 	}
+}
+
+function gameStarting(width, height){
+	if (vueSnake.thereIsEscapeDoor){
+			if (height !== 0) {
+				vueSnake.savedHeight = height + vueSnake.savedHeight;
+				console.log("La nueva altura almacenada es de: " + vueSnake.savedHeight);
+			}
+			if (width !== 0){
+				vueSnake.savedWidth = width + vueSnake.savedWidth;
+				console.log("El nuevo ancho almacenado es de: " + vueSnake.savedWidth);
+			}
+			generateEscapeDoor();
+			watchConditionsForLosing();
+			watchConditionsForWinning();
+		} else {
+			let positOfDoor = generateEscapeDoor(); //genero la puerta de salida por primera vez //traigo un json de valores x e y de la puerta, y un tercer valor que indica si está al fondo o a la derecha
+			vueSnake.escapeDoor = positOfDoor;
+			console.log("Este sería el json que traigo con los valores X e Y de la puerta: " + vueSnake.escapeDoor.x + " y " + vueSnake.escapeDoor.y);
+			vueSnake.thereIsEscapeDoor = true;
+			let dificultad = document.querySelector("#dificultad").value; //viene la dificultad ingresada por el usuario expresada en un numero
+			renderAllTheObstacles(dificultad-1); //el numero debe decir la cantidad de obstáculos en el juego
+			vueSnake.obstaclesSetUp = true;
+		}
+	console.log("El ancho que se sumó por param es de " + width);
+	console.log("La altura que se sumó por param es de " + height);
 }
 
 function deleteSnake(){
@@ -71,23 +82,28 @@ function moveSnake(direction, quantity){
 	deleteSnake();
 	keepingAllTheObstaclesInCanvas();
 	if (direction === "up" || direction === "down"){
+		vueSnake.vertical = true;
+		vueSnake.height = 40;
+		vueSnake.width = 10;
 		let height = quantity;
 		if (direction === "down"){
-			setTimeout(createSnake, 100, 0, height);//en el setInterval o setTimeOut 1° param es para function, 2do param para milisegundos, y 4°, 5° y otros params para params de la function
+			setTimeout(createSnake, 100, 0, height, direction);//en el setInterval o setTimeOut 1° param es para function, 2do param para milisegundos, y 4°, 5° y otros params para params de la function
 			
 		} else if (direction === "up"){
 			let negHeight = Number(height * (-1));
-			setTimeout(createSnake, 100, 0, negHeight);
+			setTimeout(createSnake, 100, 0, negHeight, direction);
 		}
-		
 	} else {
+		vueSnake.vertical = false;
+		vueSnake.height = 10;
+		vueSnake.width = 40;
 		let width = quantity;
 		if (direction === "right"){
-			setTimeout(createSnake, 100, width, 0);
+			setTimeout(createSnake, 100, width, 0, direction);
 			
 		} else if (direction === "left"){
 			let negWidth = Number(width * (-1));
-			setTimeout(createSnake, 100, negWidth, 0);
+			setTimeout(createSnake, 100, negWidth, 0, direction);
 		}
 		
 	}
@@ -109,18 +125,18 @@ function generateEscapeDoor(){
 	if (vueSnake.thereIsEscapeDoor) { // si existe...
 		for (let elem of vueSnake.escapeDoor){
 			if (elem.side === "right") {
-			let blueDoorRect = ctx.fillRect(elem.x, elem.y, 10, 40);
+			let blueDoorRect = ctx.fillRect(elem.x, elem.y, vueSnake.doorWidth, vueSnake.doorHeight);
 			} else { 
-			let blueDoorRect = ctx.fillRect(elem.x, elem.y, 40, 10);
+			let blueDoorRect = ctx.fillRect(elem.x, elem.y, vueSnake.doorWidth, vueSnake.doorHeight);
 			} 
 		}
 		 
 	} else { //si no existe aun
 		let obj = doorPosit(); //traigo un array con las posiciones X e Y de la door
 		if (obj.side === "bottom"){ //por la condicion que puse en doorPosit, X solo puede ser = a 397 si cae en el rightSide */
-			let blueDoorRect = ctx.fillRect(obj.x, obj.y, 40, 10);
+			let blueDoorRect = ctx.fillRect(obj.x, obj.y, vueSnake.doorWidth, vueSnake.doorHeight);
 		} else {
-			let blueDoorRect = ctx.fillRect(obj.x, obj.y, 10, 40);
+			let blueDoorRect = ctx.fillRect(obj.x, obj.y, vueSnake.doorWidth, vueSnake.doorHeight);
 		}
 		return obj; //la retorno
 	}
@@ -130,27 +146,29 @@ function generateEscapeDoor(){
 function doorPosit(){
 	let randomSide = getRandomInt(); // traigo un 1 o un 2
 	if (randomSide == 1) {
+		vueSnake.doorWidth = 40;
+		vueSnake.doorHeight = 10;
 		let y = 397;
 		let x = randomCanvasNum(399);
-		/* if (x == 397) {
-			x += 1; //esto lo hago para evitar que pueda quedar en 397 y se pueda cumplir la condicion de control en generateEscapeDoor
-		} */
-		let bottomSide = [{
-			"x": x,
-			"y": y,
-			"side": "bottom"
-		}]
+		let bottomSide = doorSide(x, y, "bottom");
 		return bottomSide;
 	} else {
+		vueSnake.doorWidth = 10;
+		vueSnake.doorHeight = 40;
 		let x = 397;
 		let y = randomCanvasNum(399);
-		let rightSide = [{
-			"x": x,
-			"y": y,
-			"side": "right"
-		}]
+		let rightSide = doorSide(x, y, "right");
 		return rightSide;
 	}
+}
+
+function doorSide(x, y, side){
+	let sideD = [{
+		"x": x,
+		"y": y,
+		"side": side
+	}]
+	return sideD;
 }
 
 function getRandomInt() {
@@ -161,16 +179,16 @@ function getRandomInt() {
 	return randNumb;
 }
 
-console.log("holaSet 7.3");
+console.log("holaSet 8.3");
 
 function generateObstacles(obstacleAlreadySetUp){
 	ctx.fillStyle = "black";
 	if (!(vueSnake.obstaclesSetUp)) {
 	let objPos = randomPositionForCanvas(); //traigo un objeto con nombres "x" e "y" y posiciones aleatorias para el tablero
-	let blackObstacle = ctx.fillRect(objPos.x, objPos.y, 30, 30);
+	let blackObstacle = ctx.fillRect(objPos.x, objPos.y, vueSnake.obstWH, vueSnake.obstWH);
 	return objPos;
 	} else {
-	let blackObstacle = ctx.fillRect(obstacleAlreadySetUp.x, obstacleAlreadySetUp.y, 30, 30); //esto es para mantener los valores de los obstáculos
+	let blackObstacle = ctx.fillRect(obstacleAlreadySetUp.x, obstacleAlreadySetUp.y, vueSnake.obstWH, vueSnake.obstWH); //esto es para mantener los valores de los obstáculos
 	}
 }
 
@@ -203,38 +221,77 @@ function randomPositionForCanvas(){
 }
 
 function watchConditionsForLosing(){ //revisa las condiciones de derrota, si el jugador perdió
-	for (const elem of vueSnake.obsPositions) {
-		let bool = areasComparison(vueSnake.savedWidth, elem.x, vueSnake.savedHeight, elem.y, vueSnake.savedHeight, elem.y, 10, 30, 30, 40);
+	console.log(vueSnake.obsPositions);
+	let bool = false;
+	for (let elem of vueSnake.obsPositions) {
+		bool = areasComparisonL(elem);
 		if (bool) {
-			console.log("El jugador ha perdido contra un obstáculo");
-			vueSnake.isTheGameOver = true;			
+		console.log("El jugador ha perdido contra un obstáculo");
+		vueSnake.isTheGameOver = true;			
 		}
 	}
+	
 }
 
 function watchConditionsForWinning(){ //revisa las condiciones de victoria, si el jugador ganó
+	let bool = false;
 	for (let elem of vueSnake.escapeDoor){
-		if (elem.side === "bottom"){
-			let bool = areasComparison(vueSnake.savedWidth, elem.x, 0, 0, vueSnake.savedHeight, elem.y, 10, 40, 0, 40);
-			if (bool) {
-				console.log("El jugador ha ganado. La serpiente ha encontrado la salida");
-				vueSnake.isTheGameOver = true;			
-			}
-		} else {
-			let bool = areasComparison(vueSnake.savedHeight, elem.y, 0, 0, vueSnake.savedWidth, elem.x, 40, 40, 0, 10);
-			if (bool) {
-				console.log("El jugador ha ganado. La serpiente ha encontrado la salida");
-				vueSnake.isTheGameOver = true;			
-			}
+		bool = areasComparisonW(elem);
+		if (bool) {
+		console.log("El jugador ha ganado. La serpiente ha encontrado la salida");
+		vueSnake.isTheGameOver = true;			
 		}
+	}
+	
+}
+
+function areasComparisonW(door){ //se encarga de medir condiciones de victoria o derrota recibiendo valores por param
+	let xComp = xComparisonW(door.x);
+	let yComp = yComparisonW(door.y);
+	if (xComp && yComp){
+		return true;
+	} else {
+		return false;
 	}
 }
 
-function areasComparison(measure1, measure2, measure3, measure4, measure5, measure6, add1, add2, add3, add4){ //se encarga de medir condiciones de victoria o derrota recibiendo valores por param
-	if (measure1 + add1 >= measure2 && measure1 <= measure2 + add2){
-		if (measure3 <= measure4 + add3 && measure5 + add4 >= measure6){
-			return true;
-		}
+function areasComparisonL(obsObj){
+	let xComp = xComparisonL(obsObj.x);
+	let yComp = yComparisonL(obsObj.y);
+	if (xComp && yComp){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function xComparisonL(obsX){
+	if (vueSnake.savedWidth + vueSnake.width >= obsX && vueSnake.savedWidth <= obsX + vueSnake.obstWH){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function yComparisonL(obsY){
+	if (vueSnake.savedHeight + vueSnake.height >= obsY && vueSnake.savedHeight <= obsY + vueSnake.obstWH){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function xComparisonW(doorX){
+	if (vueSnake.savedWidth + vueSnake.width >= doorX && vueSnake.savedWidth <= doorX + vueSnake.doorWidth){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function yComparisonW(doorY){
+	if (vueSnake.savedHeight + vueSnake.height >= doorY){
+		return true;
 	} else {
 		return false;
 	}
